debbe48dfbf55d98bba4f9853270c31a1627d9fc
parthjoshi90 <parthjoshi90@gmail.com>
2018-04-02 19:04:06 +0530

    Update urls.py

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -23,8 +23,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace='posts')),
-    url(r'^login/$', 'django.contrib.auth.views.login')
-    #url(r'^posts/$', "<app_name>.views.<function_name>"),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
 ]
 
 if settings.DEBUG:
